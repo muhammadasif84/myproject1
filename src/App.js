376cd7416f9eb584b50a556fb6aa5ecd738f7e85
Muhammad Asif <mohammedasif80902@gmail.com>
2020-06-20 20:55:14 +0500

    Update App.js
    default

--- src/App.js
+++ src/App.js
@@ -3,23 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+  let [count, setCount] = useState(0)
+ 
+  return  (
+      <div>
+        <h3>Value of counter variable is: {count}</h3>
+        <br/>
+        <button onClick={
+          ()=> setCount(count +1)
+          }>
+            Update Counter</button>
+
+      </div>
+
   );
 }
 
